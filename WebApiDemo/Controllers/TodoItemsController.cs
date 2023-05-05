e150660b53aa114c38601840b4f10cc13d4a8680
Kz12138 <804411280@qq.com>
2023-05-05 17:42:51 +0800

    添加！！！！

---
+++ WebApiDemo/Controllers/TodoItemsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+			byte[] buffer = new byte[4096];
+			using (FileStream fs = new FileStream("D:\\ChinaChess\\Chinesechess-master\\index.html", FileMode.Open, FileAccess.Read))
+            {
+          
+                int bytesRead;
+                while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0) { }
+            }
+            return File(buffer, "application/octet-stream", System.Web.HttpUtility.UrlDecode("下载.html"));
+			//if (_context.TodoItems == null)
+   //       {
+   //           return NotFound();
+   //       }
+   //         return await _context.TodoItems.ToListAsync();
+        }
+		[HttpGet("download")]
+		public FileResult downloadRequest()
+		{
+			//var addrUrl = webRootPath + "/upload/thumb.jpg";
+			var addrUrl = "C:\\Users\\80441\\Desktop\\Test\\123.png";
+
+			var stream = System.IO.File.OpenRead(addrUrl);
+
+			string fileExt = Path.GetExtension("thumb.jpg");
+
+			//获取文件的ContentType
+
+			var provider = new FileExtensionContentTypeProvider();
+
+			var memi = provider.Mappings[fileExt];
+
+			return File(stream, memi, Path.GetFileName(addrUrl));
+		}
+
+		// GET: api/TodoItems/5
+		[HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            if (_context.TodoItems == null)
+            {
+                return Problem("Entity set 'TodoContext.TodoItems'  is null.");
+            }
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            if (_context.TodoItems == null)
+            {
+                return NotFound();
+            }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return (_context.TodoItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
